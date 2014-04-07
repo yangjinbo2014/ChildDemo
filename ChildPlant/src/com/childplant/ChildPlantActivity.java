282a47c205db793648f35e1212d770c1c752616a
yangjinbo <yangjinbo@topwise3g.com>
2014-04-07 23:08:34 +0800

    修改飞虫的控制类与View分开

--- ChildPlant/src/com/childplant/ChildPlantActivity.java
+++ ChildPlant/src/com/childplant/ChildPlantActivity.java
@@ -11,9 +11,10 @@ import com.example.childplant.R;
 
 public class ChildPlantActivity extends Activity
 {
-	FlyView flyView;
+	FlyControl flyControl;
 	ImageView catchButton;
 	ImageView againButton;
+	View mFlyView;
 	
 	public static int WIDTH = 800;
 	public static int HEIGHT = 480;
@@ -23,42 +24,49 @@ public class ChildPlantActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_plant);
-        FrameLayout view = (FrameLayout)findViewById(R.id.fly_region);
-        flyView = new FlyView(this, WIDTH, HEIGHT);
-//        setContentView(flyView);
-        view.addView(flyView);
-        flyView.setDuration(30);
-        flyView.setFlySpeed(32);
+        initFly();
         catchButton = (ImageView) findViewById(R.id.catch_button);
         catchButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-					flyView.FlyCatch();
+					flyControl.flyCatch();
 			}
 		});
         
         againButton = (ImageView) findViewById(R.id.again_button);
         againButton.setOnClickListener(new OnClickListener() {
-			
+        	
 			@Override
 			public void onClick(View v) {
-					flyView.FlyStart();
+					flyControl.flyStart();
 			}
 		});
     }
     
+    private void initFly(){
+        FrameLayout view = (FrameLayout)findViewById(R.id.fly_region);
+        mFlyView = new View(this);
+        flyControl = new FlyControl(this,mFlyView, WIDTH, HEIGHT);
+//        setContentView(flyView);
+        view.addView(mFlyView, WIDTH, HEIGHT);
+        flyControl.setDuration(30);
+        flyControl.setFlySpeed(32);
+        int flag = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+//        android.provider.Settings.System.putInt(getContentResolver(), "status_bar_auto_hide", 1);
+        getWindow().getDecorView().setSystemUiVisibility(flag);
+    }
     
     @Override
     public void onResume(){
     	super.onResume();
-    	flyView.FlyStart();
+    	flyControl.flyStart();
     }
     
     @Override
     public void onPause() {
     	super.onPause();
-    	flyView.FlyStop();
+    	flyControl.flyStop();
     }
     
 }
